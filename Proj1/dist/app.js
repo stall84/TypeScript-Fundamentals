b7fbef0af7e66e46194b44ef4b6566afc970a6c0
Michael <stall84@gmail.com>
2021-02-06 11:10:55 -0500

    Finsished the input class, started on render class

--- Proj1/dist/app.js
+++ Proj1/dist/app.js
@@ -1,4 +1,23 @@
 "use strict";
+function validate(validateableInput) {
+    let isValid = true;
+    if (validateableInput.required) {
+        isValid = isValid && validateableInput.value.toString().trim().length !== 0;
+    }
+    if (validateableInput.minLength != null && typeof validateableInput.value === 'string') {
+        isValid = isValid && validateableInput.value.length >= validateableInput.minLength;
+    }
+    if (validateableInput.maxLength != null && typeof validateableInput.value === 'string') {
+        isValid = isValid && validateableInput.value.length <= validateableInput.maxLength;
+    }
+    if (validateableInput.min != null && typeof validateableInput.value === 'number') {
+        isValid = isValid && validateableInput.value >= validateableInput.min;
+    }
+    if (validateableInput.max != null && typeof validateableInput.value === 'number') {
+        isValid = isValid && validateableInput.value <= validateableInput.max;
+    }
+    return isValid;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -12,11 +31,48 @@ class ProjectInput {
         this.configure();
         this.attach();
     }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        const titleValidateable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descValidateable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        const peopleValidateable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        };
+        if (!validate(titleValidateable) ||
+            !validate(descValidateable) ||
+            !validate(peopleValidateable)) {
+            alert('Incorrect input format, please try again');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+    clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
     submitHandler(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
-        console.log(this.descriptionInputElement.value);
-        console.log(this.peopleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+        }
+        this.clearInputs();
     }
     configure() {
         this.element.addEventListener('submit', this.submitHandler.bind(this));
